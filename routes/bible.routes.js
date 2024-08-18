b61fa5a6c6a1b06354412cc76890c3e4b7fc2ef1
254binaryninja <arnoldmusandu@gmail.com>
2024-08-18 19:23:08 +0300

    change the http methods

--- routes/bible.routes.js
+++ routes/bible.routes.js
@@ -3,7 +3,7 @@ import { getResponse,getContent } from '../controllers/bible.controllers.js';
 
 const router = express.Router();
 
-router.get('/',getResponse)
+router.post('/',getResponse)
 router.get('/daily-content/',getContent)
 
 export default router
